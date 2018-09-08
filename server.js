e96aebf7d30ef8cb843b9016faba7b7be780c679
PradnyaBagave <pradnya.bagave@gmail.com>
2018-09-08 14:40:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,59 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const api = require('./api.js');
-const port = process.env.PORT || '3000';
-const app = express();
+'use strict';
 
-// define middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next();
+let db
+const dbconnection = 'mongodb://username:password@ds035336.mlab.com:35333/your-database-name';
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect(dbconnection, (err, database) => {
+    if (err) return console.log(err)
+    db = database
+    app.listen(3000, () => {
+        console.log('MONGODB listening on 3000')
+    })
+})
+
+
+function databaseStore(message) {
+    let storeData = { chatMessage: message, timestamp: new Date().getTime() }
+    db.collection('chatroom-chats').save(storeData, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+    })
+}
+
+app.get('/', (req, res) => {
+    // res.sendFile(__dirname + '/index.html')
+    res.send('Chat Server')
+})
+
+
+
+
+io.on('connection', (socket) => {
+
+    console.log('user connected');
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+
+    socket.on('add-message', (message) => {
+        io.emit('message', { type: 'new-message', text: message });
+        // stores message in database
+        databaseStore(message)
+    });
+
 });
-app.use('/', api);
-app.set('port', port);
 
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on port ${port}`));
+
+
+http.listen(5000, () => {
+    console.log('Server started on port 5000');
+});
